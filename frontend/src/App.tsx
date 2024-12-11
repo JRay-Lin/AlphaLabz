5d7f137b4d58b12b36e9d1c329da55b9cecc76da
Chun Jui, JRay , Lin <lin299579@gmail.com>
2024-12-11 23:44:00 +0800

    Login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,25 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Pages
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+
+function AppRouter() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
 
 function App() {
     return (
         <>
-            <p>Hello world</p>
+            <AppRouter />
         </>
     );
 }
