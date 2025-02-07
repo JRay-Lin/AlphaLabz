ac6a497795fbe66dd25b699482f388b473e2533a
Chun Jui, JRay , Lin <lin299579@gmail.com>
2025-02-07 13:35:39 +0800

    fetchUserList now return list content that user have permission to view

--- backend/pkg/pocketbase/fetchUserList.go
+++ backend/pkg/pocketbase/fetchUserList.go
@@ -4,20 +4,28 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 // User represents a user record from PocketBase
 type User struct {
-	Id              string `json:"id"`
-	Email           string `json:"email"`
-	EmailVisibility bool   `json:"emailVisibility"`
-	Verified        bool   `json:"verified"`
-	Name            string `json:"name"`
-	Avatar          string `json:"avatar"`
-	Role            string `json:"role"`
-	Gender          string `json:"gender"`
-	Created         string `json:"created"`
-	Updated         string `json:"updated"`
+	Id    string `json:"id,omitempty"`
+	Email string `json:"email,omitempty"`
+	// Verified bool   `json:"verified,omitempty"`
+	Name   string `json:"name,omitempty"`
+	Avatar string `json:"avatar,omitempty"`
+	// Role    string `json:"role,omitempty"`
+	Expand  expandRole `json:"expand,omitempty"`
+	Gender  string     `json:"gender,omitempty"`
+	Created string     `json:"created,omitempty"`
+	Updated string     `json:"updated,omitempty"`
+}
+
+type expandRole struct {
+	Role struct {
+		Id   string `json:"id,omitempty"`
+		Name string `json:"name,omitempty"`
+	}
 }
 
 // ListUsersResponse represents the PocketBase API response for listing users
@@ -29,8 +37,13 @@ type ListUsersResponse struct {
 }
 
 // ListUsers fetches the list of users
-func (p *PocketBaseClient) ListUsers(token string) (userList []User, totalUsers int, err error) {
-	url := fmt.Sprintf("%s/api/collections/users/records", p.BaseURL)
+func (pbClient *PocketBaseClient) ListUsers(fields []string) (userList []User, totalUsers int, err error) {
+	url := fmt.Sprintf("%s/api/collections/users/records?expand=role&", pbClient.BaseURL)
+
+	// Add fields as query parameters if specified
+	if len(fields) > 0 {
+		url += "&fields=" + strings.Join(fields, ",")
+	}
 
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
