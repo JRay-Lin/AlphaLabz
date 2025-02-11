47dfa1a313ef9e2044fab78553c68ec33403dbda
krabhi1 <abhishek33gl@gmail.com>
2025-02-11 21:12:56 +0530

    update Signup form

--- frontend/src/app/signout/Signup.tsx
+++ frontend/src/app/signout/Signup.tsx
@@ -4,31 +4,59 @@ import { Input } from "@/components/ui/input"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { format } from "date-fns"
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { cn } from "@/lib/utils"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { CalendarIcon, Link, UploadIcon } from "lucide-react"
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { useState } from "react"
 
-const formSchema = z.object({
-  email: z.string().email(),
-  roleId: z.string(),
-  roleName: z.string(),
-  name: z.string().min(3).max(100),
-  password: z.string().min(8).max(100),
-})
+const formSchema = z
+  .object({
+    email: z.string().email(),
+    roleName: z.string(),
+    name: z.string().min(3).max(100),
+    password: z.string().min(8).max(100),
+    confirmPassword: z.string().min(8).max(100),
+    gender: z.enum(["male", "female"]).optional(),
+    dob: z.date().optional(),
+    avatar: z.instanceof(File).optional(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  })
 export default function Signup() {
+  const [avatarUrl, setAvatarUrl] = useState<string>()
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       email: "default@gmail.com",
-      roleId: "1",
       roleName: "admin",
       password: "",
       name: "",
+      confirmPassword: "",
     },
   })
   const { handleSubmit } = form
@@ -40,8 +68,8 @@ export default function Signup() {
       <div className="w-full max-w-sm">
         <div className="flex flex-col gap-6">
           <Card>
-            <CardHeader>
-              <CardTitle  className="text-2xl text-center">Sign up</CardTitle>
+            <CardHeader className="py-3">
+              <CardTitle className="text-2xl text-center">Sign up</CardTitle>
             </CardHeader>
             <CardContent>
               <Form {...form}>
@@ -49,51 +77,42 @@ export default function Signup() {
                   <div className="flex flex-col gap-3">
                     <FormField
                       control={form.control}
-                      name="email"
+                      name="avatar"
                       render={({ field }) => (
-                        <FormItem className="grid gap-1">
-                          <FormLabel>Email</FormLabel>
+                        <FormItem className="flex justify-center items-center">
                           <FormControl>
-                            <Input disabled {...field} />
+                            <Avatar
+                              className="h-12 w-12"
+                              onClick={() => {
+                                const input = document.createElement("input")
+                                input.type = "file"
+                                input.accept = "image/*"
+                                input.onchange = (e: any) => {
+                                  const file = e.target.files?.[0]
+                                  if (file) {
+                                    setAvatarUrl(URL.createObjectURL(file))
+                                    field.onChange(file)
+                                  }
+                                }
+                                input.click()
+                                input.remove()
+                              }}
+                            >
+                              <AvatarImage src={avatarUrl} />
+                              <AvatarFallback>
+                                <UploadIcon size={18} className="opacity-50" />
+                              </AvatarFallback>
+                            </Avatar>
                           </FormControl>
                           <FormMessage />
                         </FormItem>
                       )}
                     />
-                    <div className="flex  gap-4">
-                      <FormField
-                        control={form.control}
-                        name="roleName"
-                        render={({ field }) => (
-                          <FormItem className="grid gap-1">
-                            <FormLabel>Role name</FormLabel>
-                            <FormControl>
-                              <Input disabled {...field} />
-                            </FormControl>
-                            <FormMessage />
-                          </FormItem>
-                        )}
-                      />
-                      <FormField
-                        control={form.control}
-                        name="roleId"
-                        render={({ field }) => (
-                          <FormItem className="grid gap-1">
-                            <FormLabel>Role ID</FormLabel>
-                            <FormControl>
-                              <Input disabled {...field} />
-                            </FormControl>
-                            <FormMessage />
-                          </FormItem>
-                        )}
-                      />
-                    </div>
-
                     <FormField
                       control={form.control}
                       name="name"
                       render={({ field }) => (
-                        <FormItem className="grid gap-2">
+                        <FormItem className="grid ">
                           <FormLabel>Name</FormLabel>
                           <FormControl>
                             <Input {...field} />
@@ -104,13 +123,129 @@ export default function Signup() {
                     />
                     <FormField
                       control={form.control}
-                      name="password"
+                      name="email"
                       render={({ field }) => (
-                        <FormItem className="grid gap-2">
-                          <FormLabel>Password</FormLabel>
+                        <FormItem className="grid ">
+                          <FormLabel>Email</FormLabel>
                           <FormControl>
-                            <Input type="password" {...field} />
+                            <Input disabled {...field} />
                           </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <div className="flex gap-4 items-start">
+                      <FormField
+                        control={form.control}
+                        name="roleName"
+                        render={({ field }) => (
+                          <FormItem className="grid flex-1">
+                            <FormLabel>Role name</FormLabel>
+                            <FormControl>
+                              <Input disabled {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="gender"
+                        render={({ field }) => (
+                          <FormItem className="grid flex-1">
+                            <FormLabel>Gender</FormLabel>
+                            <Select
+                              onValueChange={(value) => {
+                                field.onChange(value ? value : undefined)
+                              }}
+                              value={field.value}
+                            >
+                              <FormControl>
+                                <SelectTrigger>
+                                  <SelectValue placeholder="Gender" />
+                                </SelectTrigger>
+                              </FormControl>
+                              <SelectContent>
+                                <SelectItem value={null!}>None</SelectItem>
+                                <SelectItem value="male">Male</SelectItem>
+                                <SelectItem value="female">Female</SelectItem>
+                              </SelectContent>
+                            </Select>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+
+                    <div className="flex gap-4 items-start">
+                      <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                          <FormItem className="grid flex-1">
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                              <Input type="password" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="confirmPassword"
+                        render={({ field }) => (
+                          <FormItem className="grid flex-1">
+                            <FormLabel>Confirm Password</FormLabel>
+                            <FormControl>
+                              <Input type="password" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+                    <FormField
+                      control={form.control}
+                      name="dob"
+                      render={({ field }) => (
+                        <FormItem className="flex flex-col">
+                          <FormLabel>Date of birth</FormLabel>
+                          <Popover>
+                            <PopoverTrigger asChild>
+                              <FormControl>
+                                <Button
+                                  variant={"outline"}
+                                  className={cn(
+                                    "pl-3 text-left font-normal",
+                                    !field.value && "text-muted-foreground"
+                                  )}
+                                >
+                                  {field.value ? (
+                                    format(field.value, "PPP")
+                                  ) : (
+                                    <span>Pick a date</span>
+                                  )}
+                                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                </Button>
+                              </FormControl>
+                            </PopoverTrigger>
+                            <PopoverContent
+                              className="w-auto p-0"
+                              align="start"
+                            >
+                              <Calendar
+                                mode="single"
+                                selected={field.value}
+                                onSelect={field.onChange}
+                                disabled={(date) =>
+                                  date > new Date() ||
+                                  date < new Date("1900-01-01")
+                                }
+                                initialFocus
+                              />
+                            </PopoverContent>
+                          </Popover>
                           <FormMessage />
                         </FormItem>
                       )}
