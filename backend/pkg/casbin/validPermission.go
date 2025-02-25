bb53c5716351230066134b8efeab4f306b09dd83
Chun Jui, JRay , Lin <lin299579@gmail.com>
2025-02-25 11:10:07 +0800

    VerifyPermission now use JWT to fetch instead of userRoleId

--- backend/pkg/casbin/validPermission.go
+++ backend/pkg/casbin/validPermission.go
@@ -1,19 +1,26 @@
 package casbin
 
 import (
+	"alphalabz/pkg/pocketbase"
 	"fmt"
 )
 
 // CheckPermission validates user actions using Casbin
-func (ce *CasbinEnforcer) VerifyPermission(roleId string, permissionConfig PermissionConfig) (bool, error) {
+func (ce *CasbinEnforcer) VerifyPermission(pbClient *pocketbase.PocketBaseClient, rawJwtToken string, permissionConfig PermissionConfig) (bool, error) {
 	if ce.Enforcer == nil {
 		return false, fmt.Errorf("casbin Enforcer is not initialized")
 	}
 
-	ok, err := ce.Enforcer.Enforce(roleId, permissionConfig.Resources, permissionConfig.Actions, permissionConfig.Scopes)
+	userRole, err := pbClient.FetchUserInfo(rawJwtToken)
+	if err != nil {
+		return false, nil
+	}
+
+	ok, err := ce.Enforcer.Enforce(userRole.Role, permissionConfig.Resources, permissionConfig.Actions, permissionConfig.Scopes)
 	if err != nil {
 		fmt.Println("Error enforcing policy:", err)
 		return false, err
 	}
+
 	return ok, nil
 }
