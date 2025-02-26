447502d711c1331a058c9fee010c872607546f7b
Chun Jui, JRay , Lin <lin299579@gmail.com>
2025-02-26 17:56:16 +0800

    now VerifyJWTPermission also check if user have full permission("*")

--- backend/pkg/casbin/validPermission.go
+++ backend/pkg/casbin/validPermission.go
@@ -6,6 +6,8 @@ import (
 )
 
 // CheckPermission validates user actions using Casbin
+//
+// Require Resources, Actions and Scopes to be provided in PermissionConfig
 func (ce *CasbinEnforcer) VerifyJWTPermission(pbClient *pocketbase.PocketBaseClient, rawJwtToken string, permissionConfig PermissionConfig) (bool, error) {
 	if ce.Enforcer == nil {
 		return false, fmt.Errorf("casbin Enforcer is not initialized")
@@ -16,6 +18,17 @@ func (ce *CasbinEnforcer) VerifyJWTPermission(pbClient *pocketbase.PocketBaseCli
 		return false, nil
 	}
 
+	// Check if the user has the '*' scope (unrestricted access).
+	starScopeCheck, err := ce.Enforcer.Enforce(userRole.RoleId, permissionConfig.Resources, permissionConfig.Actions, "*")
+	if err != nil {
+		fmt.Println("Error enforcing policy:", err)
+		return false, err
+	}
+	if starScopeCheck {
+		return true, nil
+	}
+
+	// Check permission using the specified scope.
 	ok, err := ce.Enforcer.Enforce(userRole.RoleId, permissionConfig.Resources, permissionConfig.Actions, permissionConfig.Scopes)
 	if err != nil {
 		fmt.Println("Error enforcing policy:", err)
