91486471dc7ed1de886f2427e74e0b0066aaaa83
Chun Jui, JRay , Lin <lin299579@gmail.com>
2025-02-27 14:55:14 +0800

    remove unused code and create custom api endpoint for updating SMTP settings

--- database/main.go
+++ database/main.go
@@ -12,18 +12,6 @@ import (
 	"github.com/pocketbase/pocketbase/core"
 )
 
-type userRole struct {
-	Name string `json:"name"`
-	Role string `json:"role"`
-}
-
-// type Role struct {
-// 	Id          string
-// 	Name        string
-// 	Discription string
-// 	Permission  string
-// }
-
 // verifyRequester ensure only superuser can access the endpoints
 func verifyRequester(header string, app *pocketbase.PocketBase) error {
 	if header == "" {
@@ -56,63 +44,34 @@ func main() {
 		// serves static files from the provided public dir (if exists)
 		se.Router.GET("/{path...}", apis.Static(os.DirFS("./pb_public"), false))
 
-		// Return user's role
-		se.Router.GET("/api/role/{token}", func(e *core.RequestEvent) error {
+		se.Router.PATCH("/api/settings/smtp", func(e *core.RequestEvent) error {
 			// Hanlde requester permission
 			if err := verifyRequester(e.Request.Header.Get("Authorization"), app); err != nil {
 				return e.String(http.StatusUnauthorized, err.Error())
 			}
 
-			// Grant token from parameter
-			token := e.Request.PathValue("token")
-			user, err := app.FindAuthRecordByToken(token, core.TokenTypeAuth)
-			if err != nil {
-				return e.String(http.StatusUnauthorized, "Unauthorized")
-			} else if user.Collection().Name != "users" {
-				return e.String(http.StatusForbidden, "Unauthorized")
+			data := struct {
+				Service     string `json:"service"`
+				Host        string `json:"host"`
+				Port        int    `json:"port"`
+				Username    string `json:"username"`
+				Password    string `json:"password"`
+				FromAddress string `json:"from_address"`
+				FromName    string `json:"from_name"`
+			}{}
+			if err := e.BindBody(&data); err != nil {
+				return e.BadRequestError("Failed to read request data", err)
 			}
 
-			userRecord, err := app.FindRecordById("users", user.Id)
-			if err != nil {
-				return e.String(http.StatusInternalServerError, "Internal Server error")
-			}
+			e.App.Settings().SMTP.Host = data.Host
+			e.App.Settings().SMTP.Port = data.Port
+			e.App.Settings().SMTP.Username = data.Username
+			e.App.Settings().SMTP.Password = data.Password
+			e.App.Settings().Meta.SenderAddress = data.FromAddress
+			e.App.Settings().Meta.SenderName = data.FromName
 
-			return e.JSON(http.StatusOK, map[string]any{"name": userRecord.GetString("name"), "role": userRecord.GetString("role")})
+			return e.JSON(http.StatusOK, map[string]any{"message": "ok"})
 		})
-
-		// se.Router.POST("/api/role/copy/{roleId}", func(e *core.RequestEvent) error {
-		// 	if err := verifyRequester(e.Request.Header.Get("Authorization"), app); err != nil {
-		// 		return e.String(http.StatusUnauthorized, err.Error())
-		// 	}
-
-		// 	roleId := e.Request.PathValue("roleId")
-		// 	role, err := app.FindRecordById("roles", roleId)
-		// 	if err != nil {
-		// 		return e.String(http.StatusNotFound, "Role not found")
-		// 	}
-		// 	roleName := role.GetString("name")
-		// 	roleDiscription := role.GetString("discription")
-		// 	rolePermission := role.GetString("permission")
-
-		// 	// Create new role
-		// 	collection, err := app.FindCollectionByNameOrId("roles")
-		// 	if err != nil {
-		// 		return err
-		// 	}
-
-		// 	record := core.NewRecord(collection)
-
-		// 	record.Set("name", roleName)
-		// 	record.Set("discription", roleDiscription)
-		// 	record.Set("permission", rolePermission)
-
-		// 	err = app.Save(record)
-		// 	if err != nil {
-		// 		return err
-		// 	}
-
-		// 	return e.JSON(http.StatusCreated, map[string]any{"newRoleId": "role"})
-		// })
 
 		return se.Next()
 	})
